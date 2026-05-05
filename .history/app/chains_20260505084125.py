fcf24846bfb81cdd7372c4d6b5305f3f6c7d57f1
annintech-cmyk <ann.intech@gmail.com>
2026-05-05 08:42:31 +0200

    first commit

--- .history/app/chains_20260505084125.py
+++ .history/app/chains_20260505084125.py
@@ -45,7 +45,7 @@ class Chain:
             process optimization, cost reduction, and heightened overall efficiency. 
             Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
             in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase InTech's portfolio: {link_list}
+            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
             Remember you are Anna, BDE at InTech. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
--- .history/app/chains_20260505084142.py
+++ .history/app/chains_20260505084142.py
@@ -39,13 +39,13 @@ class Chain:
             {job_description}
 
             ### INSTRUCTION:
-            You are Anna, a business development executive at InTech. InTech is an AI & Software Consulting company dedicated to facilitating
+            You are Anna, a business development executive at XXTech. InTech is an AI & Software Consulting company dedicated to facilitating
             the seamless integration of business processes through automated tools. 
             Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
             process optimization, cost reduction, and heightened overall efficiency. 
             Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
             in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase InTech's portfolio: {link_list}
+            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
             Remember you are Anna, BDE at InTech. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
